a97e41de9812144e9f15cc0fe00adc917abf8745
rafvai <raf_22@hotmail.it>
2024-08-20 20:19:19 +0200

    Add .gitignore to exclude sensitive and unnecessary files

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,24 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
-import os
 from dotenv import load_dotenv
 
 # Load environment variables from a .env file
 load_dotenv()
-
-secret_key = os.getenv("SECRET_KEY")
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__, template_folder="templates")
     
-    # Set the database URI
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Raf/Desktop/DietShopper/ultimate.db'
+    # Load the secret key from environment variables
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key')
+
+    # Load the database URI from environment variables, with a fallback to the SQLite database path
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///C:/Users/Raf/Desktop/DietShopper/ultimate.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
-    # Set the secret key for session security
-    app.secret_key = secret_key
-
     # Set the session type
     app.config["SESSION_PERMANENT"] = False
     app.config["SESSION_TYPE"] = "filesystem"
