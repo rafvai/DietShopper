1855447af99fcfdd9852258e3ffe08e7badb9117
rafvai <raf_22@hotmail.it>
2024-08-23 18:54:57 +0200

    Modifies run.py to get the por from the env var

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
+import os
 from app import create_app
-
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    # Get the port from the environment variable or use 5000 by default
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
